d024fd3bfa1c63d5a599c2383b32ff4619e93892
Paryul10 <paryul10101010@gmail.com>
2019-05-07 13:51:45 +0530

    Login , Logout , Outputs function points

--- portal/portalapp/models.py
+++ portal/portalapp/models.py
@@ -3,8 +3,9 @@ from django.conf import settings
 from django.contrib.auth.models import User
 
 # Create your models here. 
-class Points(models.Model):
+class Student(models.Model):
     # username = models.CharField(max_length=50)
-    username = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete= models.CASCADE, blank = True, null = True)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    # username = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete= models.CASCADE, blank = True, null = True)
     function_points = models.IntegerField(default=0)
     effort = models.IntegerField(default=0)
--- portal/portalapp/views.py
+++ portal/portalapp/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from .models import Points
+from .models import Student
+from django.contrib.auth.models import User
 from django.contrib.auth import views as auth_views
 
 # Create your views here.
@@ -10,14 +11,18 @@ def index(request):
 
 
 def displayusers(request):
-    students = Points.objects.all()
+    students = User.objects.all()
     # usernames = ', ' .join(s.username for s in students)
     context = {'students':students}
     # return  HttpResponse(usernames)
     return render(request,'portalapp/user.html',context)
 
 def displaypoints(request,username):
-    p = Points.objects.get(pk=username).function_points
+    current_user = request.user
+    # print(current_user)
+    # print("---------------------------------------------------")
+    p = Student.objects.get(user=current_user).function_points
+    # print("---------------------------------------------------")
     # e = Points.objects.get(pk=user_id).effort
     return HttpResponse("Function Points of user %s" % p)
 
