c1265ca8da3935f789305a1261ff1d357d4c3cbd
Paryul10 <paryul10101010@gmail.com>
2019-05-13 17:00:58 +0530

    Added cloning script

---
+++ codeit.sh
@@ -0,0 +1,18 @@
+# https://github.com/Paryul10/SPRING19
+
+base="https://github.com"
+handle=$1
+repo=$2
+commitid=$3
+end=".git"
+
+url="$base/$handle/$repo$end"
+
+echo $url
+
+git clone $url
+cd $repo
+git reset --hard $commitid
+
+cd ..
+cloc $repo
