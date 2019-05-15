1fec0febd297213fe4f876b32597d1a78a9a0e5e
Paryul10 <paryul10101010@gmail.com>
2019-05-15 16:51:13 +0530

    Added Evalution Script

--- codeit.sh
+++ codeit.sh
@@ -14,8 +14,38 @@ git clone $url
 cd $repo
 git reset --hard $commitid
 
+var=0
+
+if [ "$repo" == "computer-organization-iiith" ]
+then
+    var=1
+elif [ "$repo" == "computer-graphics-iiith" ]
+then
+    var=2
+elif [ "$repo" == "pattern-recognition-iiith" ]
+then
+    var=3
+elif [ "$repo" == "vlsi-iiith" ]
+then
+    var=4
+elif [ "$repo" == "digital-logic-design-iiith" ]
+then
+    var=5
+elif [ "$repo" == "speech-signal-processing-iiith" ]
+then
+    var=6
+else
+    echo "No repo Matching.! Please check inputs"
+fi
+
+# echo $var
+
+
 cd ..
-cloc $repo > abc.txt
-#echo $output 
+
+echo $var > abc.txt
+cloc $repo >> abc.txt
+# rm abc.txt
+# cloc $repo
 
 rm -rf $repo
--- script.py
+++ script.py
@@ -2,18 +2,39 @@ import re
 
 script_1 = open("abc.txt", "r")
 
-for line in script_1:
+lines = script_1.readlines()
+# print(lines[0])
+lab = int(lines[0],10)
+# print(lab)
+a = []
+b = []
+c = []
+
+aflag = 0
+bflag = 0
+cflag = 0
+
+for line in lines:
 
 	if re.match('JavaScript', line):
-
 		a = line.split()
-
+		aflag = 1
 	if re.match('HTML', line):
-
 		b = line.split()
-
+		bflag = 1
 	if re.match('CSS', line):
-
 		c = line.split()
+		cflag = 1
 
-x = [a[0], a[-1], b[0], b[-1], c[0], c[-1]]
+x = []
+if aflag:
+	x.append(a[0])
+	x.append(a[-1])
+if bflag:
+	x.append(b[0])
+	x.append(b[-1])
+if cflag:
+	x.append(c[0])
+	x.append(c[-1])
+
+# print(x)
